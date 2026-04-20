8c2bb152b478be00acd0973b4371aedb3b93b31a
txystc <tianxiangyue1991@gmail.com>
2026-04-20 17:35:13 +0800

    Diagnose: capture Step 7 output to step7_output.txt with HOME=/root

---
+++ git_push3.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d G:\QLAW\sme_strategy
+"C:\Program Files\Git\cmd\git.exe" config user.email "tianxiangyue1991@gmail.com"
+"C:\Program Files\Git\cmd\git.exe" config user.name "txystc"
+"C:\Program Files\Git\cmd\git.exe" push -u origin master
+pause
